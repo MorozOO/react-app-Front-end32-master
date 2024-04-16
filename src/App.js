2582479fab97edcb34044ae0f23bb351d1c39593
MorozOO <Vx7nneN8508B>
2024-04-16 18:45:45 +0300

    Lesson3 Create Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import First from './components/First';
+import TodoList from './components/todo/Todo-list';
+
+function App() {
+  return (
+    <TodoList />
+  );
+}
+
+export default App;
